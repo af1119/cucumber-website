77e9bc6be22de5ce73b29a956252784811e583ee
afiori5 <117650908+afiori5@users.noreply.github.com>
2024-11-24 12:10:01 -0500

    pushing frontend

---
+++ fastapi_routes.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+# No need for fastapi at the moment - test frontend first
+
+#app = FastAPI()
+
+
+#@app.get("/")
+#async def root():
+#    return {"message": "Hello World"}
+
